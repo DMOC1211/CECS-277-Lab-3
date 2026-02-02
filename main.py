5a998c0ec262650ae0d8bd699d541dbc0eaacf38
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-02-02 12:31:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,19 @@ def create_board(rows, cols):
         board.append(list)
     return board
 
+def place_mines (board,mines):
+    
+
+def count_mines(board):
+    mines = 0 
+    if 
+
 
 def main():
 
-    x = check_input.get_int_range("Enter number of rows:", 5, 10)
-    y = check_input.get_int_range("Enter number of columns:", 5, 10)
+    x = check_input.get_int_range("Enter number of rows: (5-10)", 5, 10)
+    y = check_input.get_int_range("Enter number of columns: (5-10)", 5, 10)
+    mines_placed = check_input.get_int_range("Enter number of mines: (5-10)", 5, 10)
     new_board = create_board(x, y)
     print(new_board)
 
